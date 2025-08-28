8c75b77b491d7941cf26e4fe0776de0c9133e031
Abhishek Jain <abhishek@Abhisheks-MacBook-Pro.local>
2025-08-28 12:14:21 +0530

    Added the auth, client and user details api

---
+++ examples/example.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Abhi13027/go-arrow/arrow"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	godotenv.Load()
+	userID := os.Getenv("USER_ID")
+	password := os.Getenv("PASSWORD")
+	totp_key := os.Getenv("TOTP_KEY")
+	appID := os.Getenv("APP_ID")
+	appSecret := os.Getenv("APP_SECRET")
+
+	client := arrow.NewClient(appID, appSecret)
+
+	err := client.AutoLogin(userID, password, totp_key)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("Login successful!")
+
+	// Get user details
+	user, err := client.GetUserDetails()
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+
+	}
+
+	fmt.Printf("User Details: %+v\n", user)
+
+}
